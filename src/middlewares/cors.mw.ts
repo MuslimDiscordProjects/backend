4387fead61cfea732d0682903ccc7e2a86cdcd81
Adam Amer <adamamer1801@gmail.com>
2022-08-07 17:42:52 +0300

    Fixed the headers
    Added the wrong names 🤦‍♂️

--- src/middlewares/cors.mw.ts
+++ src/middlewares/cors.mw.ts
@@ -1,10 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 
 export function cors(req: Request, res: Response, next: NextFunction) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 }
