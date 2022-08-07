a94e258da6e42c8de03db9f0fcfd22318bbf6c7f
Adam Amer <adamamer1801@gmail.com>
2022-08-07 17:58:07 +0300

    I HATE THE CREATORS OF CORS

--- src/middlewares/cors.mw.ts
+++ src/middlewares/cors.mw.ts
@@ -3,6 +3,5 @@ import { NextFunction, Request, Response } from "express";
 export function cors(req: Request, res: Response, next: NextFunction) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 }
