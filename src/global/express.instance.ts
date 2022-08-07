eb987a2a40fc85b003802cd43ee4280048d5d130
Adam Amer <adamamer1801@gmail.com>
2022-08-07 17:32:34 +0300

    Added cors middleware to add cors header on every request :thumbsup:

--- src/global/express.instance.ts
+++ src/global/express.instance.ts
@@ -1,16 +1,16 @@
-import cors from "cors";
 import e from "express";
 import morgan from "morgan";
 import JWTValidator from "../middlewares/JWTValidator.mw";
 import router from "../routers/router";
 import multer from "multer";
+import { cors } from "../middlewares/cors.mw";
 
 const express = e();
 express
   .use(JWTValidator)
   .use(e.json())
   .use(router)
-  .use(cors())
+  .use(cors)
   .use(morgan("common"))
   .use(multer().any);
 
